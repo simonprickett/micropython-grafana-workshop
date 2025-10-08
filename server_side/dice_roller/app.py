ba13ed9d128333bf0423db514bffd40c51b32bef
Simon Prickett <simon.prickett@grafana.com>
2025-10-08 10:49:27 +0100

    Standardized on use of double quotes.

--- server_side/dice_roller/app.py
+++ server_side/dice_roller/app.py
@@ -3,14 +3,14 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/roll', methods=['GET'])
+@app.route("/roll", methods=["GET"])
 def roll_dice():
     number = random.randint(1, 6)
-    return jsonify({'number': number})
+    return jsonify({"number": number})
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return "Dice roller! Try /roll..."
+    return "Dice roller! Try <a href=\"/roll\">/roll</a>..."
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
